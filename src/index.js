ed786d094c66e2de65eab9b8459d8ca6ce3f465d
UmmNobody <naphatedit142521@gmail.com>
2024-09-13 14:18:15 +0700

    Deploy heroku

--- src/index.js
+++ src/index.js
@@ -195,5 +195,5 @@ app.post("/note/delete", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT : ${PORT}`);
+    console.log(`Server is running on PORT : ${PORT}...`);
 });
