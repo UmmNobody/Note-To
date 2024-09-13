6c2b0a9294dd10c53da46675be884bf2d7a40049
UmmNobody <naphatedit142521@gmail.com>
2024-09-13 15:17:44 +0700

    Reinstall dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const { User, Note } = require("./config");
 const { title } = require('process');
